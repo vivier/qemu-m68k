0499bf0a7f4a7689481404229f9dd8461ecdafb7
Laurent Vivier <laurent@vivier.eu>
2009-06-15 00:53:14 +0200

    m68k: on 0 bit shift, don't update X flag
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/helper.c
+++ target-m68k/helper.c
@@ -566,7 +566,7 @@ uint32_t HELPER(glue(glue(shl, bits),_cc))(CPUM68KState *env, uint32_t val, uint
         cf = 0; \
     } \
     env->cc_src = cf; \
-    env->cc_x = (cf != 0); \
+    if (shift) env->cc_x = (cf != 0); \
     env->cc_dest = result; \
     return result; \
 }
@@ -595,7 +595,7 @@ uint32_t HELPER(glue(glue(shr, bits), _cc))(CPUM68KState *env, uint32_t val, uin
         cf = 0; \
     } \
     env->cc_src = cf; \
-    env->cc_x = (cf != 0); \
+    if (shift) env->cc_x = (cf != 0); \
     env->cc_dest = result; \
     return result; \
 }
@@ -621,7 +621,7 @@ uint32_t HELPER(glue(glue(sar, bits), _cc))(CPUM68KState *env, uint32_t val, uin
         cf = (type)val >> (bits - 1); \
     } \
     env->cc_src = cf; \
-    env->cc_x = cf; \
+    if (shift) env->cc_x = (cf != 0); \
     env->cc_dest = result; \
     return result; \
 }
