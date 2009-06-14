0754b307f4e6fb09a2f076f8c82cd66d21841b5c
Laurent Vivier <laurent@vivier.eu>
2009-06-14 08:42:54 +0200

    m68k: add sbcd instruction
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/helpers.h
+++ target-m68k/helpers.h
@@ -83,4 +83,5 @@ DEF_HELPER_3(bitfield_load, i64, i32, i32, i32);
 DEF_HELPER_4(bitfield_store, void, i32, i32, i32, i64);
 
 DEF_HELPER_3(abcd_cc, i32, env, i32, i32);
+DEF_HELPER_3(sbcd_cc, i32, env, i32, i32);
 #include "def-helper.h"
