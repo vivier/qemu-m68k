76877f38c2ad1f3667b6b6a5f23f5be832679dc7
Laurent Vivier <laurent@vivier.eu>
2009-06-26 22:21:49 +0200

    m68k: asl/asr, clear C flag if shift count is 0
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/helper.c
+++ target-m68k/helper.c
@@ -720,7 +720,7 @@ uint32_t HELPER(glue(glue(sal, bits),_cc))(CPUM68KState *env, uint32_t val, uint
     }\
     if (shift == 0) { \
         result = (type)val; \
-        cf = env->cc_src & CCF_C; \
+        cf = 0; \
     } else if (shift < bits) { \
         result = (type)val << shift; \
         cf = ((type)val >> (bits - shift)) & 1; \
@@ -749,7 +749,7 @@ uint32_t HELPER(glue(glue(sar, bits), _cc))(CPUM68KState *env, uint32_t val, uin
     shift &= 63; \
     if (shift == 0) { \
         result = (type)val; \
-        cf = (env->cc_src & CCF_C) != 0; \
+        cf = 0; \
     } else if (shift < bits) { \
         result = (type)val >> shift; \
         cf = ((type)val >> (shift - 1)) & 1; \
