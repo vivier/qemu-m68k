3f081a1376b239713755367b688ec0629cc9b7ab
Laurent Vivier <laurent@vivier.eu>
2011-01-15 18:03:16 +0100

    m68k: remove dead code
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -4314,9 +4314,6 @@ gen_intermediate_code_internal(CPUM68KState *env, TranslationBlock *tb,
         tb->size = dc->pc - pc_start;
         tb->icount = num_insns;
     }
-
-    //optimize_flags();
-    //expand_target_qops();
 }
 
 void gen_intermediate_code(CPUM68KState *env, TranslationBlock *tb)
