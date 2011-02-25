4443a9ef246e4c8a4f5663059cf291d7dfd04c32
Laurent Vivier <laurent@vivier.eu>
2011-02-25 23:48:01 +0100

    m68k: correct addsubq
    DEST_EA() must use same opsize as SRC_EA().
    
    This allows to run nano and vim.
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -2274,7 +2274,7 @@ DISAS_INSN(addsubq)
         }
         gen_update_cc_add(dest, src2);
     }
-    DEST_EA(insn, OS_LONG, dest, &addr);
+    DEST_EA(insn, opsize, dest, &addr);
 }
 
 DISAS_INSN(tpf)
