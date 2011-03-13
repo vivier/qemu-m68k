54f4ee82efc1ee266a45b11f84c0db7aeb08c672
Laurent Vivier <laurent@vivier.eu>
2011-03-13 04:09:36 +0100

    m68k: correct fpcr update
    allow to run metacity
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -3602,7 +3602,7 @@ DISAS_INSN(fpu)
         case 4: /* FPCR */
             SRC_EA(val, OS_LONG, 0, NULL);
             gen_helper_set_fpcr(cpu_env, val);
-            break;
+            return;
         case 1: /* FPIAR */
         case 2: /* FPSR */
         default:
@@ -3613,7 +3613,6 @@ DISAS_INSN(fpu)
     case 5: /* fmove from control register.  */
         switch ((ext >> 10) & 7) {
         case 4: /* FPCR */
-            /* Not implemented.  Always return zero.  */
             DEST_EA(insn, OS_LONG, QEMU_FPCR, NULL);
             return;
         case 1: /* FPIAR */
