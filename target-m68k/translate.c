7c5ca8fe52eebc7316be8acfee1cbddd10181b3d
Laurent Vivier <laurent@vivier.eu>
2011-03-15 00:28:25 +0100

    m68k: flush flags before negx instruction.
    Force the computing of the flags before negx as
    negx will keep Z flag from previous instructions
    if result is non zero.
    
    Seen with gcc testsuite,
    gcc.c-torture/execute/builtin-bitops-1.c
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -1895,6 +1895,7 @@ DISAS_INSN(negx)
     int opsize;
 
     opsize = insn_opsize(insn, 6);
+    gen_flush_flags(s);
     SRC_EA(src, opsize, -1, &addr);
     dest = tcg_temp_new();
     switch(opsize) {
