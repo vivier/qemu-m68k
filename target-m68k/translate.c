5d5e75ce1ee782549f118b5c1d6b9ff9a119e240
Laurent Vivier <laurent@vivier.eu>
2011-04-03 23:58:04 +0200

    m68k: add ftanh instruction
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -3737,6 +3737,9 @@ DISAS_INSN(fpu)
     case 4: case 0x41: case 0x45: /* fsqrt */
         gen_helper_sqrt_FP0(cpu_env);
         break;
+    case 0x09:                       /* ftanh */
+        gen_helper_tanh_FP0(cpu_env);
+        break;
     case 0x0a:                       /* fatan */
         gen_helper_atan_FP0(cpu_env);
         break;
