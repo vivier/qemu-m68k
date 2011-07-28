6707274d0150d74d71bdade5c22bc0d2ae8a017b
Laurent Vivier <Laurent@Vivier.EU>
2011-07-28 14:45:01 +0200

    linux-user,m68k: display default cpu
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- target-m68k/helper.c
+++ target-m68k/helper.c
@@ -52,6 +52,11 @@ static void m68k_cpu_list_entry(gpointer data, gpointer user_data)
 
     typename = object_class_get_name(c);
     name = g_strndup(typename, strlen(typename) - strlen("-" TYPE_M68K_CPU));
+    if (strcmp(name, TARGET_DEFAULT_CPU) == 0) {
+        (*s->cpu_fprintf)(s->file, " >");
+    } else {
+        (*s->cpu_fprintf)(s->file, "  ");
+    }
     (*s->cpu_fprintf)(s->file, "%s\n",
                       name);
     g_free(name);
