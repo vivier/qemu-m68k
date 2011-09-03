57e26148bd15a8c7c9d5815065d289d737e8718e
Laurent Vivier <laurent@vivier.eu>
2011-09-03 09:48:52 +0200

    m68k: interrupt level 7 is non maskable

--- cpu-exec.c
+++ cpu-exec.c
@@ -472,9 +472,9 @@ int cpu_exec(CPUState *env)
                         next_tb = 0;
                     }
 #elif defined(TARGET_M68K)
-                    if (interrupt_request & CPU_INTERRUPT_HARD
-                        && ((env->sr & SR_I) >> SR_I_SHIFT)
-                            < env->pending_level) {
+                    if (interrupt_request & CPU_INTERRUPT_HARD &&
+                        ((((env->sr & SR_I) >> SR_I_SHIFT) <
+                          env->pending_level) || env->pending_level == 7)) {
                         /* Real hardware gets the interrupt vector via an
                            IACK cycle at this point.  Current emulated
                            hardware doesn't rely on this, so we
