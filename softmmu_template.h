effef5d0cdfd7308a94e1242ab9c79a668e97ed6
Laurent Vivier <laurent@vivier.eu>
2011-09-09 23:31:12 +0200

    m68k: update SSW on MMU fault
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- softmmu_template.h
+++ softmmu_template.h
@@ -152,6 +152,19 @@ glue(glue(glue(HELPER_PREFIX, ld), SUFFIX), MMUSUFFIX)(ENV_PARAM
     } else {
         /* the page is not in the TLB : fill it */
         retaddr = GETPC();
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
 #ifdef ALIGNED_ONLY
         if ((addr & (DATA_SIZE - 1)) != 0)
             do_unaligned_access(ENV_VAR addr, READ_ACCESS_TYPE, mmu_idx, retaddr);
@@ -208,6 +221,19 @@ glue(glue(slow_ld, SUFFIX), MMUSUFFIX)(ENV_PARAM
         }
     } else {
         /* the page is not in the TLB : fill it */
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
         tlb_fill(env, addr, READ_ACCESS_TYPE, mmu_idx, retaddr);
         goto redo;
     }
@@ -298,6 +324,19 @@ void glue(glue(glue(HELPER_PREFIX, st), SUFFIX), MMUSUFFIX)(ENV_PARAM
     } else {
         /* the page is not in the TLB : fill it */
         retaddr = GETPC();
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
 #ifdef ALIGNED_ONLY
         if ((addr & (DATA_SIZE - 1)) != 0)
             do_unaligned_access(ENV_VAR addr, 1, mmu_idx, retaddr);
@@ -352,6 +391,19 @@ static void glue(glue(slow_st, SUFFIX), MMUSUFFIX)(ENV_PARAM
         }
     } else {
         /* the page is not in the TLB : fill it */
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
         tlb_fill(env, addr, 1, mmu_idx, retaddr);
         goto redo;
     }
