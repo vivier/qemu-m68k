804f12ac60c193cb29a76dc9fbdf64bdefdfc274
Laurent Vivier <laurent@vivier.eu>
2011-09-09 23:31:12 +0200

    m68k: update SSW on MMU fault
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- softmmu_template.h
+++ softmmu_template.h
@@ -155,6 +155,19 @@ WORD_TYPE helper_le_ld_name(CPUArchState *env, target_ulong addr, int mmu_idx,
     /* If the TLB entry is for a different page, reload and try again.  */
     if ((addr & TARGET_PAGE_MASK)
          != (tlb_addr & (TARGET_PAGE_MASK | TLB_INVALID_MASK))) {
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
 #ifdef ALIGNED_ONLY
         if ((addr & (DATA_SIZE - 1)) != 0) {
             cpu_unaligned_access(ENV_GET_CPU(env), addr, READ_ACCESS_TYPE,
@@ -244,6 +257,19 @@ WORD_TYPE helper_be_ld_name(CPUArchState *env, target_ulong addr, int mmu_idx,
         if ((addr & (DATA_SIZE - 1)) != 0) {
             cpu_unaligned_access(ENV_GET_CPU(env), addr, READ_ACCESS_TYPE,
                                  mmu_idx, retaddr);
+        }
+#endif
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
         }
 #endif
         tlb_fill(ENV_GET_CPU(env), addr, READ_ACCESS_TYPE, mmu_idx, retaddr);
@@ -363,6 +389,19 @@ void helper_le_st_name(CPUArchState *env, target_ulong addr, DATA_TYPE val,
     /* If the TLB entry is for a different page, reload and try again.  */
     if ((addr & TARGET_PAGE_MASK)
         != (tlb_addr & (TARGET_PAGE_MASK | TLB_INVALID_MASK))) {
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
 #ifdef ALIGNED_ONLY
         if ((addr & (DATA_SIZE - 1)) != 0) {
             cpu_unaligned_access(ENV_GET_CPU(env), addr, 1, mmu_idx, retaddr);
@@ -439,6 +478,19 @@ void helper_be_st_name(CPUArchState *env, target_ulong addr, DATA_TYPE val,
     /* If the TLB entry is for a different page, reload and try again.  */
     if ((addr & TARGET_PAGE_MASK)
         != (tlb_addr & (TARGET_PAGE_MASK | TLB_INVALID_MASK))) {
+#ifdef TARGET_M68K
+        switch (DATA_SIZE) {
+        case 1:
+            env->mmu.ssw = M68K_BA_SIZE_BYTE;
+            break;
+        case 2:
+            env->mmu.ssw = M68K_BA_SIZE_WORD;
+            break;
+        case 4:
+            env->mmu.ssw = M68K_BA_SIZE_LONG;
+            break;
+        }
+#endif
 #ifdef ALIGNED_ONLY
         if ((addr & (DATA_SIZE - 1)) != 0) {
             cpu_unaligned_access(ENV_GET_CPU(env), addr, 1, mmu_idx, retaddr);
