9c9300289ea1b19543e41c2c5091542f26c4e321
Laurent Vivier <laurent@vivier.eu>
2011-09-18 00:49:58 +0200

    m68k: add move16 instruction
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -3714,6 +3714,83 @@ DISAS_INSN(ptest)
     gen_helper_ptest(cpu_env, reg, tcg_const_i32(1 - ((insn >> 5) & 1)));
 }
 
+DISAS_INSN(move16)
+{
+    int use_areg;
+    int index = IS_USER(s);
+    int i;
+    TCGv tmp;
+
+    use_areg = (insn >> 5) & 1;
+
+    if (use_areg) {
+        TCGv src;
+        TCGv dst;
+        uint16_t ext;
+
+        ext = read_im16(s);
+        if ((ext & (1 << 15)) == 0) {
+            gen_exception(s, s->pc - 2, EXCP_ILLEGAL);
+        }
+
+        src = AREG(insn, 0);
+        dst = AREG(ext, 12);
+
+        tmp = tcg_temp_new_i32();
+        for (i = 0; i < 4; i++) {
+            tcg_gen_qemu_ld32u(tmp, src, index);
+            tcg_gen_qemu_st32(tmp, dst, index);
+            tcg_gen_addi_i32(src, src, 4);
+            tcg_gen_addi_i32(dst, dst, 4);
+        }
+    } else {
+        uint32_t addr;
+        TCGv reg;
+        int mode;
+        TCGv val;
+
+        reg = AREG(insn, 0);
+        addr = read_im16(s);
+        mode = (insn >> 3) & 3;
+
+        tmp = tcg_temp_new_i32();
+        switch (mode) {
+        case 0: /* MOVE16 (Ay)+, (xxx).L */
+            for (i = 0; i < 4; i++) {
+                tcg_gen_qemu_ld32u(tmp, reg, index);
+                tcg_gen_qemu_st32(tmp, tcg_const_i32(addr + i * 4), index);
+                tcg_gen_addi_i32(reg, reg, 4);
+            }
+            break;
+        case 1: /* MOVE16 (xxx).L, (Ay)+ */
+            for (i = 0; i < 4; i++) {
+                tcg_gen_qemu_ld32u(tmp, tcg_const_i32(addr + i * 4), index);
+                tcg_gen_qemu_st32(tmp, reg, index);
+                tcg_gen_addi_i32(reg, reg, 4);
+            }
+            break;
+        case 2: /* MOVE16 (Ay), (xxx).L */
+            val = tcg_temp_new_i32();
+            tcg_gen_mov_i32(val, reg);
+            for (i = 0; i < 4; i++) {
+                tcg_gen_qemu_ld32u(tmp, val, index);
+                tcg_gen_qemu_st32(tmp, tcg_const_i32(addr + i * 4), index);
+                tcg_gen_addi_i32(val, val, 4);
+            }
+            break;
+        case 3: /* MOVE16 (xxx).L, (Ay) */
+            val = tcg_temp_new_i32();
+            tcg_gen_mov_i32(val, reg);
+            for (i = 0; i < 4; i++) {
+                tcg_gen_qemu_ld32u(tmp, tcg_const_i32(addr + i * 4), index);
+                tcg_gen_qemu_st32(tmp, val, index);
+                tcg_gen_addi_i32(val, val, 4);
+            }
+            break;
+        }
+    }
+}
+
 DISAS_INSN(wddata)
 {
     gen_exception(s, s->pc - 2, EXCP_PRIVILEGE);
@@ -4761,6 +4838,7 @@ void register_m68k_insns (CPUM68KState *env)
     INSN(cinv,      f400, ff20, M68000);
     INSN(pflush,    f500, ffe0, M68000);  /* FIXME: 68040 version only*/
     INSN(ptest,     f548, ffd8, M68000);  /* FIXME: 68040 version only*/
+    INSN(move16,    f600, ffc0, M68000);  /* FIXME: 68040 version only */
     INSN(wddata,    fb00, ff00, CF_ISA_A);
     INSN(wdebug,    fbc0, ffc0, CF_ISA_A);
 #ifdef CONFIG_EMULOP
