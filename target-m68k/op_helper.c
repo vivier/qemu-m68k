7f59489bb574b1aa3b2177a4d7c776581db74b7f
Laurent Vivier <Laurent@Vivier.EU>
2012-05-01 22:26:24 +0200

    m68k: on exception, correctly save old SR instead of new one
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- target-m68k/op_helper.c
+++ target-m68k/op_helper.c
@@ -206,7 +206,6 @@ static void do_interrupt_all(int is_hw)
 
     sp = env->aregs[7];
 
-
     /*
      * MC68040UM/AD,  chapter 9.3.10
      */
@@ -225,12 +224,9 @@ static void do_interrupt_all(int is_hw)
     m68k_switch_sp(env);
     sp = env->aregs[7];
 
-    /* ??? This could cause MMU faults.  */
-
     if (m68k_feature(env, M68K_FEATURE_M68000)) {
         sp &= ~1;
         if (env->exception_index == 2) {
-            /* FIXME */
             sp -= 4;
             stl_kernel(sp, 0); /* push data 3 */
             sp -= 4;
@@ -261,15 +257,15 @@ static void do_interrupt_all(int is_hw)
             stw_kernel(sp, env->mmu.ssw); /* special status word */
             sp -= 4;
             stl_kernel(sp, env->mmu.ar); /* effective address */
-            do_stack_frame(&sp, 7, env->sr, 0, retaddr);
+            do_stack_frame(&sp, 7, oldsr, 0, retaddr);
         } else if (env->exception_index == 3) {
-            do_stack_frame(&sp, 2, env->sr, 0, retaddr);
+            do_stack_frame(&sp, 2, oldsr, 0, retaddr);
         } else if (env->exception_index == 5 ||
                    env->exception_index == 6 ||
                    env->exception_index == 7 ||
                    env->exception_index == 9) {
             /* FIXME: addr is not only env->pc */
-            do_stack_frame(&sp, 2, env->sr, env->pc, retaddr);
+            do_stack_frame(&sp, 2, oldsr, env->pc, retaddr);
         } else if (is_hw && env->exception_index >= 24 &&
                    env->exception_index < 32) {
             do_stack_frame(&sp, 0, oldsr, 0, retaddr);
@@ -282,13 +278,13 @@ static void do_interrupt_all(int is_hw)
                 do_stack_frame(&sp, 1, oldsr, 0, retaddr);
             }
         } else {
-            do_stack_frame(&sp, 0, env->sr, 0, retaddr);
+            do_stack_frame(&sp, 0, oldsr, 0, retaddr);
         }
     } else {
         fmt |= 0x40000000;
         fmt |= (sp & 3) << 28;
         fmt |= vector << 16;
-        fmt |= env->sr;
+        fmt |= oldsr;
 
         sp &= ~3;
         sp -= 4;
