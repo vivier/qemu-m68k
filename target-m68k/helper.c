4e3d0e0c3f8a9ae51a11095b9daaed71adcb7d90
Laurent Vivier <Laurent@Vivier.EU>
2012-05-10 20:59:15 +0200

    m68k: rework mmu.ssw update
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- target-m68k/helper.c
+++ target-m68k/helper.c
@@ -688,8 +688,6 @@ static int check_TTR(uint32_t ttr, hwaddr *physical, int *prot,
 do { \
     next = ldl_phys(cs->as, entry); \
     if ((next & 2) == 0) { \
-        env->mmu.ssw |= M68K_ATC_040; \
-        env->mmu.ssw |= access_type & ACCESS_STORE ? 0 : M68K_RW_040; \
         return -1; /* INVALID */ \
     } \
     if ((next & (1 << 3)) == 0) { \
@@ -702,7 +700,6 @@ do { \
         } \
         *prot &= ~PAGE_WRITE; \
         if (access_type & ACCESS_STORE) { \
-            env->mmu.ssw |= M68K_ATC_040; \
             return -1; \
         } \
     } \
@@ -773,8 +770,6 @@ TTR_exit:
      next = ldl_phys(cs->as, entry);
 
     if ((next & 3) == 0) {
-        env->mmu.ssw |= M68K_ATC_040;
-        env->mmu.ssw |= access_type & ACCESS_STORE ? 0 : M68K_RW_040;
         return -1;
     }
     if ((next & 3) == 2) {
@@ -806,15 +801,12 @@ TTR_exit:
         }
         *prot &= ~PAGE_WRITE;
         if (access_type & ACCESS_STORE) {
-            env->mmu.ssw |= M68K_ATC_040;
             return -1;
         }
     }
     if (next & (1 << 7)) {
         /* SUPERVISOR */
         if ((access_type & ACCESS_SUPER) == 0) {
-            env->mmu.ssw |= M68K_ATC_040;
-            env->mmu.ssw |= access_type & ACCESS_STORE ? 0 : M68K_RW_040;
             return -1;
         }
     }
@@ -898,10 +890,22 @@ int m68k_cpu_handle_mmu_fault(CPUState *cs, vaddr address, int rw,
                      prot, mmu_idx, page_size);
         return 0;
     }
+    /* page fault */
+    env->mmu.ssw |= M68K_ATC_040;
+    if (access_type & ACCESS_SUPER) {
+        env->mmu.ssw |= 4;
+    }
+    if ((access_type & ACCESS_CODE) == 0) {
+        env->mmu.ssw |= 2;
+    } else {
+        env->mmu.ssw |= 1;
+    }
     env->mmu.wb3_status = 0;
     if (access_type & ACCESS_STORE) {
         env->mmu.wb3_status = env->mmu.ssw & 0x7f;
         env->mmu.wb3_status |= 0x80; /* valid */
+    } else {
+        env->mmu.ssw |= M68K_RW_040;
     }
     env->mmu.ar = address;
     cs->exception_index = EXCP_ACCESS;
