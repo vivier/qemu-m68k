521f9af759cd8926f5cf885d0915293fa507ed71
Laurent Vivier <Laurent@Vivier.EU>
2012-05-10 20:47:16 +0200

    m68k: set correct page size in tlb_set_page()
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- target-m68k/helper.c
+++ target-m68k/helper.c
@@ -822,7 +822,7 @@ do { \
 
 static int get_physical_address(CPUState *env, target_phys_addr_t *physical,
                                 int *prot, target_ulong address,
-                                int access_type)
+                                int access_type, target_ulong *page_size)
 {
     uint32_t tia;
     uint32_t tib;
@@ -850,6 +850,7 @@ TTR_exit:
                 /* Transparent Translation Register bit */
                 env->mmu.mmusr |= 1 << 1;
             }
+            *page_size = TARGET_PAGE_SIZE;
             return 0;
         }
     }
@@ -929,9 +930,11 @@ TTR_exit:
     }
 
     if (env->mmu.tcr & 0x4000) {
+        *page_size = 8192;
         page_offset = address & 0x1fff;
         *physical = (next & ~0x1fff) + page_offset;
     } else {
+        *page_size = 4096;
         page_offset = address & 0x0fff;
         *physical = (next & ~0x0fff) + page_offset;
     }
@@ -944,6 +947,7 @@ target_phys_addr_t cpu_get_phys_page_debug(CPUState *env, target_ulong addr)
     target_phys_addr_t phys_addr;
     int prot;
     int access_type;
+    target_ulong page_size;
 
     if ((env->mmu.tcr & (1 << 15)) == 0) {
         /* MMU disabled */
@@ -954,7 +958,7 @@ target_phys_addr_t cpu_get_phys_page_debug(CPUState *env, target_ulong addr)
     if (env->sr & SR_S)
         access_type |= ACCESS_SUPER;
     if (get_physical_address(env, &phys_addr, &prot,
-                             addr, access_type) != 0) {
+                             addr, access_type, &page_size) != 0) {
         return -1;
     }
     return phys_addr;
@@ -967,6 +971,7 @@ int cpu_m68k_handle_mmu_fault (CPUState *env, target_ulong address, int rw,
     int prot;
     int access_type;
     int ret;
+    target_ulong page_size;
 
     if ((env->mmu.tcr & (1 << 15)) == 0) {
         /* MMU disabled */
@@ -992,11 +997,11 @@ int cpu_m68k_handle_mmu_fault (CPUState *env, target_ulong address, int rw,
     }
 
     ret = get_physical_address(env, &physical, &prot,
-                               address, access_type);
+                               address, access_type, &page_size);
     if (ret == 0) {
         tlb_set_page(env, address & TARGET_PAGE_MASK,
                      physical & TARGET_PAGE_MASK,
-                     prot, mmu_idx, TARGET_PAGE_SIZE);
+                     prot, mmu_idx, page_size);
         return 0;
     }
     env->mmu.wb3_status = 0;
@@ -2598,6 +2603,7 @@ void HELPER(ptest)(CPUState * env, uint32_t addr, uint32_t is_write)
     int ret;
     int access_type;
     int prot;
+    target_ulong page_size;
 
     access_type = ACCESS_PTEST;
     access_type |= (env->dfc & 4) ? ACCESS_SUPER : 0;
@@ -2607,7 +2613,8 @@ void HELPER(ptest)(CPUState * env, uint32_t addr, uint32_t is_write)
 
     env->mmu.mmusr = 0;
     env->mmu.ssw = 0;
-    ret = get_physical_address(env, &physical, &prot, addr, access_type);
+    ret = get_physical_address(env, &physical, &prot, addr,
+                               access_type, &page_size);
     if (ret == 0) {
         env->mmu.mmusr |= 1; /* RESIDENT */
         env->mmu.mmusr |= physical & 0xfffff000;
