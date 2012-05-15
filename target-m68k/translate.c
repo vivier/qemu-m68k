fbe7201b7e59c2863758e8278acb6c12c70faf8f
Laurent Vivier <Laurent@Vivier.EU>
2012-05-15 22:17:51 +0200

    m68k: is_mem is useless
    Remove is_mem as it is never tested anymore since:
    
        commit bfa50bc2638d877cf2900712b7503be22e8811cb
        Author: aliguori <aliguori@c046a42c-6fe2-441c-8c8c-71466251a162>
        Date:   Tue Nov 18 20:26:41 2008 +0000
    
            Remove premature memop TB terminations (Jan Kiszka)
    
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -129,7 +129,6 @@ typedef struct DisasContext {
     uint32_t fpcr;
     struct TranslationBlock *tb;
     int singlestep_enabled;
-    int is_mem;
     TCGv_i64 mactmp;
     int done_mac;
 } DisasContext;
@@ -249,7 +248,6 @@ static inline TCGv gen_load(DisasContext *s, int opsize, TCGv addr,
                             int sign, int index)
 {
     TCGv tmp;
-    s->is_mem = 1;
     tmp = tcg_temp_new_i32();
     switch(opsize) {
     case OS_BYTE:
@@ -278,7 +276,6 @@ static inline TCGv gen_load(DisasContext *s, int opsize, TCGv addr,
 static inline void gen_store(DisasContext *s, int opsize, TCGv addr, TCGv val,
                              int index)
 {
-    s->is_mem = 1;
     switch(opsize) {
     case OS_BYTE:
         tcg_gen_qemu_st8(val, addr, index);
@@ -856,7 +853,6 @@ static inline void gen_load_FP0(DisasContext * s, int opsize, TCGv addr)
 {
     TCGv tmp;
     int index = IS_USER(s);
-    s->is_mem = 1;
     switch(opsize) {
     case OS_BYTE:
         tcg_gen_qemu_ld8s(QREG_FP0H, addr, index);
@@ -904,7 +900,6 @@ static inline void gen_store_FP0(DisasContext *s, int opsize, TCGv addr)
 {
     TCGv tmp;
     int index = IS_USER(s);
-    s->is_mem = 1;
     switch(opsize) {
     case OS_BYTE:
         gen_helper_reds32_FP0(cpu_env);
@@ -4991,7 +4986,6 @@ gen_intermediate_code_internal(M68kCPU *cpu, TranslationBlock *tb,
     dc->singlestep_enabled = cs->singlestep_enabled;
     dc->fpcr = env->fpcr;
     dc->user = (env->sr & SR_S) == 0;
-    dc->is_mem = 0;
     dc->done_mac = 0;
     lj = -1;
     num_insns = 0;
