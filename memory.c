9a5e8b92986b50ebb99cd8be60bb3961340ca2af
Laurent Vivier <Laurent@Vivier.EU>
2012-08-01 22:24:54 +0200

    Add big-endian support in access_with_adjusted_size()
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- memory.c
+++ memory.c
@@ -334,6 +334,7 @@ static void access_with_adjusted_size(target_phys_addr_t addr,
                                       unsigned size,
                                       unsigned access_size_min,
                                       unsigned access_size_max,
+                                      enum device_endian endianness,
                                       void (*access)(void *opaque,
                                                      target_phys_addr_t addr,
                                                      uint64_t *value,
@@ -354,10 +355,37 @@ static void access_with_adjusted_size(target_phys_addr_t addr,
     }
     access_size = MAX(MIN(size, access_size_max), access_size_min);
     access_mask = -1ULL >> (64 - access_size * 8);
-    for (i = 0; i < size; i += access_size) {
-        /* FIXME: big-endian support */
-        access(opaque, addr + i, value, access_size, i * 8, access_mask);
+#ifdef HOST_WORDS_BIGENDIAN
+    if (endianness == DEVICE_LITTLE_ENDIAN
+#ifdef TARGET_WORDS_LITTLEENDIAN
+        || endianness == DEVICE_NATIVE_ENDIAN
+#endif
+       ) {
+        for (i = 0; i < size; i += access_size) {
+            access(opaque, addr + i, value, access_size,
+                   (size - access_size - i) * 8, access_mask);
+        }
+    } else {
+        for (i = 0; i < size; i += access_size) {
+            access(opaque, addr + i, value, access_size, i * 8, access_mask);
+        }
     }
+#else
+    if (endianness == DEVICE_BIG_ENDIAN
+#ifdef TARGET_WORDS_BIGENDIAN
+        || endianness == DEVICE_NATIVE_ENDIAN
+#endif
+       ) {
+        for (i = 0; i < size; i += access_size) {
+            access(opaque, addr + i, value, access_size,
+                   (size - access_size - i) * 8, access_mask);
+        }
+    } else {
+        for (i = 0; i < size; i += access_size) {
+            access(opaque, addr + i, value, access_size, i * 8, access_mask);
+        }
+    }
+#endif
 }
 
 static AddressSpace address_space_memory;
@@ -406,6 +434,7 @@ static void memory_region_iorange_read(IORange *iorange,
     access_with_adjusted_size(offset, data, width,
                               mr->ops->impl.min_access_size,
                               mr->ops->impl.max_access_size,
+                              mr->ops->endianness,
                               memory_region_read_accessor, mr);
 }
 
@@ -436,6 +465,7 @@ static void memory_region_iorange_write(IORange *iorange,
     access_with_adjusted_size(offset, &data, width,
                               mr->ops->impl.min_access_size,
                               mr->ops->impl.max_access_size,
+                              mr->ops->endianness,
                               memory_region_write_accessor, mr);
 }
 
@@ -872,6 +902,7 @@ static uint64_t memory_region_dispatch_read1(MemoryRegion *mr,
     access_with_adjusted_size(addr, &data, size,
                               mr->ops->impl.min_access_size,
                               mr->ops->impl.max_access_size,
+                              mr->ops->endianness,
                               memory_region_read_accessor, mr);
 
     return data;
@@ -926,6 +957,7 @@ static void memory_region_dispatch_write(MemoryRegion *mr,
     access_with_adjusted_size(addr, &data, size,
                               mr->ops->impl.min_access_size,
                               mr->ops->impl.max_access_size,
+                              mr->ops->endianness,
                               memory_region_write_accessor, mr);
 }
 
