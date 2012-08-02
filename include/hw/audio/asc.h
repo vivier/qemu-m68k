85f260ae3d180e52c05ff86908468abd9e878a87
Laurent Vivier <Laurent@Vivier.EU>
2012-08-02 18:03:30 +0200

    q800: Apple Sound Chip (ASC) emulation
    This is broken as the linux driver seems broken too...
    
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

---
+++ include/hw/audio/asc.h
@@ -0,0 +1,11 @@
+enum
+{
+    ASC_TYPE_ASC    = 0,  /* original discrete Apple Sound Chip */
+    ASC_TYPE_EASC   = 1,  /* discrete Enhanced Apple Sound Chip */
+    ASC_TYPE_V8     = 2,  /* Subset of ASC included in the V8 ASIC (LC/LCII) */
+    ASC_TYPE_EAGLE  = 3,  /* Subset of ASC included in the Eagle ASIC (Classic II) */
+    ASC_TYPE_SPICE  = 4,  /* Subset of ASC included in the Spice ASIC (Color Classic) */
+    ASC_TYPE_SONORA = 5,  /* Subset of ASC included in the Sonora ASIC (LCIII) */
+    ASC_TYPE_VASP   = 6,  /* Subset of ASC included in the VASP ASIC  (IIvx/IIvi) */
+    ASC_TYPE_ARDBEG = 7   /* Subset of ASC included in the Ardbeg ASIC (LC520) */
+};
