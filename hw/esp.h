2f8aa3a356b65093cfd77d72ff2cf5328e9b48b4
Laurent Vivier <Laurent@Vivier.EU>
2012-08-05 11:37:18 +0200

    ESP: add pseudo-DMA as used by Macintosh
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- hw/esp.h
+++ hw/esp.h
@@ -7,7 +7,11 @@ typedef void (*ESPDMAMemoryReadWriteFunc)(void *opaque, uint8_t *buf, int len);
 void esp_init(target_phys_addr_t espaddr, int it_shift,
               ESPDMAMemoryReadWriteFunc dma_memory_read,
               ESPDMAMemoryReadWriteFunc dma_memory_write,
-              void *dma_opaque, qemu_irq irq, qemu_irq *reset,
-              qemu_irq *dma_enable);
+              void *dma_opaque, qemu_irq irq, qemu_irq irq_data,
+              qemu_irq *reset, qemu_irq *dma_enable);
+void esp_init_pdma(target_phys_addr_t espaddr, int it_shift,
+              target_phys_addr_t pdmaaddr,
+              qemu_irq irq, qemu_irq irq_data,
+              qemu_irq *reset, qemu_irq *dma_enable);
 
 #endif
