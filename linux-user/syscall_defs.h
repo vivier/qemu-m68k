3c84b98a8c7bbf92d3c5d52331c21c9701537763
Laurent Vivier <Laurent@Vivier.EU>
2012-12-26 00:48:37 +0100

    linux-user: add socketcall() strace
    Signed-off-by: Laurent Vivier <Laurent@Vivier.EU>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -119,6 +119,34 @@ struct target_sockaddr {
     uint8_t sa_data[14];
 };
 
+struct target_sockaddr_un {
+    uint16_t su_family;
+    uint8_t sun_path[108];
+};
+
+struct target_in_addr {
+    uint32_t s_addr; /* big endian */
+};
+
+struct target_sockaddr_in {
+  uint16_t sin_family;
+  int16_t sin_port; /* big endian */
+  struct target_in_addr sin_addr;
+  uint8_t __pad[sizeof(struct target_sockaddr) -
+                sizeof(uint16_t) - sizeof(int16_t) -
+                sizeof(struct target_in_addr)];
+};
+
+struct target_sockaddr_ll {
+  uint16_t sll_family;
+  uint16_t sll_protocol; /* big endian */
+  int32_t sll_ifindex;
+  uint16_t sll_hatype;
+  uint8_t sll_pkttype;
+  uint8_t sll_halen;
+  uint8_t sll_addr[8];
+};
+
 struct target_sock_filter {
     abi_ushort code;
     uint8_t jt;
@@ -129,10 +157,6 @@ struct target_sock_filter {
 struct target_sock_fprog {
     abi_ushort len;
     abi_ulong filter;
-};
-
-struct target_in_addr {
-    uint32_t s_addr; /* big endian */
 };
 
 struct target_ip_mreq {
