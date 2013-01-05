b0f1e3be46281f27ba43b509b8e1d2178c0189de
Laurent Vivier <laurent@vivier.eu>
2013-01-05 23:15:17 +0100

    scripts: add script to create linux container (LXC)
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

---
+++ scripts/qemu-create-lxc.sh
@@ -0,0 +1,227 @@
+QEMU_PATH=$(dirname $(dirname $(readlink -f $0)))
+TARGET="$1"
+
+set_m68k() {
+	CONTAINER_NAME=virt${TARGET}
+	CONTAINER_PATH=/containers/${TARGET}
+	LXC_CONF=$HOME/lxc-${TARGET}.conf
+
+	DEBIAN_REPO=http://archive.debian.org/debian
+	DEBIAN_DIST=etch-m68k
+	DEBIAN_SIGN=55BE302B
+
+        CONFIGURE_PARAMS=--m68k-default-cpu=m68040
+	CHECK_BIN=check_m68k
+}
+
+set_alpha() {
+	CONTAINER_NAME=virt${TARGET}
+	CONTAINER_PATH=/containers/${TARGET}
+	LXC_CONF=$HOME/lxc-${TARGET}.conf
+
+	DEBIAN_REPO=http://archive.debian.org/debian
+	DEBIAN_DIST=lenny
+	DEBIAN_SIGN=473041FA
+
+        CONFIGURE_PARAMS=""
+	CHECK_BIN=check_alpha
+}
+
+check_target() {
+	case "$TARGET"
+	in
+	m68k)
+		set_m68k
+		;;
+	alpha)
+		set_alpha
+		;;
+	*)
+		echo "ERROR: unknown target $TARGET" 1>&2
+		exit 1
+		;;
+	esac
+}
+
+check_m68k() {
+	if ${QEMU_PATH}/build-m68k/m68k-linux-user/qemu-m68k -cpu help | grep -q ">m68040"
+	then
+		echo "Found an existing qemu-m68k, use it !" 1>&2
+		return 0
+	fi
+	echo "Found an existing qemu-m68k, but with no m68040 emulation" 1>&2
+	echo "Please, remove it" 1>&2
+	exit 1
+}
+
+check_alpha() {
+	test -e ${QEMU_PATH}/build-alpha/alpha-linux-user/qemu-alpha
+}
+
+installed_dpkg() {
+	dpkg --status "$1" > /dev/null 2>&1
+}
+
+check_env() {
+	for pkg in zlib1g gcc make debootstrap lxc
+	do
+		if  ! installed_dpkg $pkg
+		then
+			echo "$pkg is needed" 1>&2
+			exit 1
+		fi
+	done
+}
+
+create_qemu() {
+	if [ -e "${QEMU_PATH}/build-${TARGET}/${TARGET}-linux-user/qemu-${TARGET}" ]
+	then
+		if ${CHECK_BIN}
+		then
+			return 0
+		fi
+	fi
+	echo "cd ${QEMU_PATH} && \
+	mkdir build-${TARGET} && \
+	cd build-${TARGET} && \
+	../configure --static --suid-able ${CONFIGURE_PARAMS} \
+		     --target-list=${TARGET}-linux-user && \
+	make" | sudo -i -u ${SUDO_USER}
+	if ! ${CHECK_BIN}
+	then
+		echo "ERROR: generated qemu binary is invalid !" 1>&2
+		exit 1
+	fi
+}
+
+create_root() {
+	# sanity check
+
+	if [ $(readlink -f ${CONTAINER_PATH}/) = "/" ]
+	then
+		echo "ERROR: invalid path ${CONTAINER_PATH}" 1>&2
+		exit 1
+	fi
+
+	# check directory
+
+	if [ -e "${CONTAINER_PATH}" ]
+	then
+		echo "${CONTAINER_PATH} already exists" 1>&2
+		echo "Please, remove it" 1>&2
+		exit 1
+	fi
+
+	# Debian bootstrap
+
+	mkdir -p "${CONTAINER_PATH}"
+	debootstrap --foreign \
+		    --arch=${TARGET} \
+                    ${DEBIAN_DIST} ${CONTAINER_PATH} \
+		    ${DEBIAN_REPO} && \
+
+	# adding qemu binary
+
+	rm -fr ${CONTAINER_PATH}/usr/bin/qemu-${TARGET} && \
+	cp ${QEMU_PATH}/build-${TARGET}/${TARGET}-linux-user/qemu-${TARGET} \
+	   ${CONTAINER_PATH}/bin && \
+	chown root:root ${CONTAINER_PATH}/bin/qemu-${TARGET} && \
+	chmod +s ${CONTAINER_PATH}/bin/qemu-${TARGET} && \
+	${QEMU_PATH}/scripts/qemu-update-binfmt ${TARGET}
+
+	# debian bootstrap second stage
+
+	chroot ${CONTAINER_PATH} debootstrap/debootstrap --second-stage
+
+	# configuration
+
+	cat >> ${CONTAINER_PATH}/etc/fstab <<!EOF
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+proc		/proc		proc	nodev,noexec,nosuid 0	1
+sys		/sys		sysfs	nodev,noexec,nosuid 0	1
+devpts		/dev/pts	devpts	nodev,noexec,nosuid 0	1
+!EOF
+
+	echo "c:2345:respawn:/sbin/getty 38400 console" >> ${CONTAINER_PATH}/etc/inittab
+
+	cat >> ${CONTAINER_PATH}/etc/network/interfaces <<!EOF
+auto eth0
+iface eth0 inet dhcp
+!EOF
+
+	cat >> ${CONTAINER_PATH}/etc/apt/sources.list <<!EOF
+deb ${DEBIAN_REPO} ${DEBIAN_DIST} main contrib non-free
+deb-src ${DEBIAN_REPO} ${DEBIAN_DIST} main contrib non-free
+!EOF
+
+	rm -f ${CONTAINER_PATH}/dev/ptmx
+
+	HOME=/root chroot ${CONTAINER_PATH} gpg --keyserver pgpkeys.mit.edu --recv-key ${DEBIAN_SIGN}
+	HOME=/root chroot ${CONTAINER_PATH} gpg -a --export ${DEBIAN_SIGN} | chroot ${CONTAINER_PATH}  apt-key add -
+	chroot ${CONTAINER_PATH} apt-get update
+	chroot ${CONTAINER_PATH} dpkg-reconfigure tzdata
+	chroot ${CONTAINER_PATH} apt-get install locales
+	chroot ${CONTAINER_PATH} dpkg-reconfigure locales
+}
+
+create_lxc() {
+	cat > ${LXC_CONF} <<!EOF
+lxc.utsname = ${CONTAINER_NAME}
+
+lxc.network.type = veth
+lxc.network.flags = up
+lxc.network.link = lxcbr0
+lxc.network.name = eth0
+
+lxc.pts=1023
+lxc.tty=12
+
+lxc.rootfs = ${CONTAINER_PATH}
+
+lxc.cgroup.devices.deny = a
+lxc.cgroup.devices.allow = c 136:* rwm # pts
+lxc.cgroup.devices.allow = c 254:0 rwm # rtc
+lxc.cgroup.devices.allow = c 5:* rwm 
+lxc.cgroup.devices.allow = c 4:* rwm # ttyXX
+lxc.cgroup.devices.allow = c 1:* rwm
+lxc.cgroup.devices.allow = b 7:* rwm # loop
+lxc.cgroup.devices.allow = b 1:* rwm # ram
+
+!EOF
+	lxc-create -n ${CONTAINER_NAME} -f ${LXC_CONF}
+}
+
+check_env
+check_target
+
+echo "TARGET        : ${TARGET}"
+echo "CONTAINER_NAME: ${CONTAINER_NAME}"
+echo "CONTAINER_PATH: ${CONTAINER_PATH}"
+echo "QEMU_PATH     : ${QEMU_PATH}"
+echo "LXC_CONF      : ${LXC_CONF}"
+echo "DEBIAN_REPO   : ${DEBIAN_REPO}"
+echo "DEBIAN_DIST   : ${DEBIAN_DIST}"
+
+if lxc-list | grep -q ${CONTAINER_NAME}
+then
+	echo "${CONTAINER_NAME} already exists !" 1>&2
+	echo "you can remove it with :" 1>&2
+	echo "    sudo lxc-destroy -n ${CONTAINER_NAME}" 1>&2
+	echo "[WARNING: this will remove the directory too]" 1>&2
+	exit 2
+fi
+
+if [ "$USER" != "root" ]
+then
+	echo "You need to be root to run this command" 2>&1
+	exit 3
+fi
+
+create_qemu
+create_root
+create_lxc
+
+chroot ${CONTAINER_PATH} uname -a
+
+echo "you can now start your container using:"
+echo "    sudo lxc-start -n ${CONTAINER_NAME}"
