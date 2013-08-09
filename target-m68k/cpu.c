e568f82b9d080c4e13e8e6122d053a9ca9585b01
Laurent Vivier <laurent@vivier.eu>
2013-08-09 20:06:29 +0200

    m68k: manage unassigned acces and generate ACCESS FAULT (BUS ERR)
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/cpu.c
+++ target-m68k/cpu.c
@@ -266,11 +266,9 @@ static void m68k_cpu_class_init(ObjectClass *c, void *data)
     cc->set_pc = m68k_cpu_set_pc;
     cc->gdb_read_register = m68k_cpu_gdb_read_register;
     cc->gdb_write_register = m68k_cpu_gdb_write_register;
-#ifdef CONFIG_USER_ONLY
     cc->handle_mmu_fault = m68k_cpu_handle_mmu_fault;
-#else
+    cc->do_unassigned_access = m68k_cpu_unassigned_access;
     cc->get_phys_page_debug = m68k_cpu_get_phys_page_debug;
-#endif
     dc->vmsd = &vmstate_m68k_cpu;
     cc->gdb_num_core_regs = 18;
     cc->gdb_core_xml_file = "cf-core.xml";
