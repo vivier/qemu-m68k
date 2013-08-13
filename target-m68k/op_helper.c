b81245daad33fde94e570475e10c785c5e2d5ad9
Laurent Vivier <laurent@vivier.eu>
2013-08-13 23:14:57 +0200

    m68k: if cpu_unassigned_access() from gdb, env is NULL
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/op_helper.c
+++ target-m68k/op_helper.c
@@ -389,6 +389,10 @@ void cpu_unassigned_access(CPUM68KState *env, hwaddr addr,
     qemu_log_mask(CPU_LOG_INT, "Unassigned " TARGET_FMT_plx " wr=%d exe=%d\n",
              addr, is_write, is_exec);
 #endif
+    if (env == NULL) {
+        /* when called from gdb, env is NULL */
+        return;
+    }
 
     env->mmu.mmusr = 0;
     env->mmu.ssw = M68K_ATC_040;
