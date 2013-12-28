2c88c042cd254ea0ab560f55081bf9791a15c216
Laurent Vivier <laurent@vivier.eu>
2013-12-28 20:40:22 +0100

    q800: really detect ADB devices...
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- include/hw/input/adb.h
+++ include/hw/input/adb.h
@@ -114,6 +114,6 @@ int adb_poll(ADBBusState *s, uint8_t *buf_out);
 #define TYPE_ADB_KEYBOARD "adb-keyboard"
 #define TYPE_ADB_MOUSE "adb-mouse"
 
-void adb_send(ADBBusState *adb, int state, uint8_t data);
-void adb_receive(ADBBusState *adb, int state, uint8_t *data);
+int adb_send(ADBBusState *adb, int state, uint8_t data);
+int adb_receive(ADBBusState *adb, int state, uint8_t *data);
 #endif /* !defined(__ADB_H__) */
