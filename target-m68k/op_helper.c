a3a517866c2417ab6cc236e081bdfba9ba466eab
Laurent Vivier <laurent@vivier.eu>
2014-01-12 01:03:25 +0100

    m68k: review m68k_switch_sp()
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target-m68k/op_helper.c
+++ target-m68k/op_helper.c
@@ -93,8 +93,6 @@ throwaway:
                 sp += 52;
                 break;
             }
-            env->aregs[7] = sp;
-            m68k_switch_sp(env);
         }
     } else {
         fmt = cpu_ldl_kernel(env, sp);
@@ -102,9 +100,9 @@ throwaway:
         sp |= (fmt >> 28) & 3;
         env->sr = fmt & 0xffff;
         sp += 8;
-        env->aregs[7] = sp;
-        m68k_switch_sp(env);
     }
+    env->aregs[7] = sp;
+    m68k_switch_sp(env);
 }
 
 static inline void do_stack_frame(CPUM68KState *env, uint32_t *sp,
