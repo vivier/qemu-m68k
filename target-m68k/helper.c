tylerwhall/CECS-525-QEMU

m68k: improve addx instructions Add (byte, word) opsize Add memory access
Signed-off-by: Laurent Vivier <laurent@vivier.eu>

target-m68k/helper.c
@@ -591,7 +591,53 @@ uint32_t HELPER(subx_cc)(CPUState *env, uint32_t op1, uint32_t op2)
     return res;
 }
 
-uint32_t HELPER(addx_cc)(CPUState *env, uint32_t op1, uint32_t op2)
+uint32_t HELPER(addx8_cc)(CPUState *env, uint32_t op1, uint32_t op2)
+{
+    uint8_t res;
+    uint32_t old_flags;
+
+    old_flags = env->cc_dest;
+    if (env->cc_x) {
+        res = (uint8_t)op1 + (uint8_t)op2 + 1;
+        env->cc_x = (res <= (uint8_t)op2);
+        env->cc_op = CC_OP_ADDXB;
+    } else {
+        res = (uint8_t)op1 + (uint8_t)op2;
+        env->cc_x = (res < (uint8_t)op2);
+        env->cc_op = CC_OP_ADDB;
+    }
+    env->cc_dest = res;
+    env->cc_src = (uint8_t)op2;
+    cpu_m68k_flush_flags(env, env->cc_op);
+    /* !Z is sticky.  */
+    env->cc_dest &= (old_flags | ~CCF_Z);
+    return (op1 & 0xffffff00) | res;
+}
+
+uint32_t HELPER(addx16_cc)(CPUState *env, uint32_t op1, uint32_t op2)
+{
+    uint16_t res;
+    uint32_t old_flags;
+
+    old_flags = env->cc_dest;
+    if (env->cc_x) {
+        res = (uint16_t)op1 + (uint16_t)op2 + 1;
+        env->cc_x = (res <= (uint16_t)op2);
+        env->cc_op = CC_OP_ADDXW;
+    } else {
+        res = (uint16_t)op1 + (uint16_t)op2;
+        env->cc_x = (res < (uint16_t)op2);
+        env->cc_op = CC_OP_ADDW;
+    }
+    env->cc_dest = res;
+    env->cc_src = (uint16_t)op2;
+    cpu_m68k_flush_flags(env, env->cc_op);
+    /* !Z is sticky.  */
+    env->cc_dest &= (old_flags | ~CCF_Z);
+    return (op1 & 0xffff0000) | res;
+}
+
+uint32_t HELPER(addx32_cc)(CPUState *env, uint32_t op1, uint32_t op2)
 {
     uint32_t res;
     uint32_t old_flags;

vivier/qemu-m68k

target-m68k/helper.c
@@ -507,7 +507,53 @@ uint32_t HELPER(subx_cc)(CPUM68KState *env, uint32_t op1, uint32_t op2)
     return res;
 }
 
-uint32_t HELPER(addx_cc)(CPUM68KState *env, uint32_t op1, uint32_t op2)
+uint32_t HELPER(addx8_cc)(CPUM68KState *env, uint32_t op1, uint32_t op2)
+{
+    uint8_t res;
+    uint32_t old_flags;
+
+    old_flags = env->cc_dest;
+    if (env->cc_x) {
+        res = (uint8_t)op1 + (uint8_t)op2 + 1;
+        env->cc_x = (res <= (uint8_t)op2);
+        env->cc_op = CC_OP_ADDXB;
+    } else {
+        res = (uint8_t)op1 + (uint8_t)op2;
+        env->cc_x = (res < (uint8_t)op2);
+        env->cc_op = CC_OP_ADDB;
+    }
+    env->cc_dest = res;
+    env->cc_src = (uint8_t)op2;
+    cpu_m68k_flush_flags(env, env->cc_op);
+    /* !Z is sticky.  */
+    env->cc_dest &= (old_flags | ~CCF_Z);
+    return (op1 & 0xffffff00) | res;
+}
+
+uint32_t HELPER(addx16_cc)(CPUM68KState *env, uint32_t op1, uint32_t op2)
+{
+    uint16_t res;
+    uint32_t old_flags;
+
+    old_flags = env->cc_dest;
+    if (env->cc_x) {
+        res = (uint16_t)op1 + (uint16_t)op2 + 1;
+        env->cc_x = (res <= (uint16_t)op2);
+        env->cc_op = CC_OP_ADDXW;
+    } else {
+        res = (uint16_t)op1 + (uint16_t)op2;
+        env->cc_x = (res < (uint16_t)op2);
+        env->cc_op = CC_OP_ADDW;
+    }
+    env->cc_dest = res;
+    env->cc_src = (uint16_t)op2;
+    cpu_m68k_flush_flags(env, env->cc_op);
+    /* !Z is sticky.  */
+    env->cc_dest &= (old_flags | ~CCF_Z);
+    return (op1 & 0xffff0000) | res;
+}
+
+uint32_t HELPER(addx32_cc)(CPUM68KState *env, uint32_t op1, uint32_t op2)
 {
     uint32_t res;
     uint32_t old_flags;