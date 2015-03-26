a8ada811489139e25e93650555cc3e4dbe5971c5
Laurent Vivier <laurent@vivier.eu>
2015-03-26 22:16:03 +0100

    Revert "qemu/osdep: Remove the need for qemu_init_auxval"
    This reverts commit 2b45c3f5008bf760e628126f0344c1effbee0714.
    
    This patch is based on availability of /proc/self/auxv which is
    not in case of chroot or during the boot sequence of a container
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    
    Conflicts:
    	linux-user/main.c
    	vl.c

--- linux-user/main.c
+++ linux-user/main.c
@@ -4193,6 +4193,8 @@ int main(int argc, char **argv, char **envp)
 
     module_call_init(MODULE_INIT_QOM);
 
+    qemu_init_auxval(envp);
+
     if ((envlist = envlist_create()) == NULL) {
         (void) fprintf(stderr, "Unable to allocate envlist\n");
         exit(EXIT_FAILURE);
