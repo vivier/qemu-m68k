15418d9b0cda8184900b9b2e8d4c567682b1fb67
Laurent Vivier <laurent@vivier.eu>
2015-03-26 22:16:03 +0100

    Revert "qemu/osdep: Remove the need for qemu_init_auxval"
    This reverts commit 2b45c3f5008bf760e628126f0344c1effbee0714.
    
    This patch is based on availability of /proc/self/auxv which is
    not in case of chroot or during the boot sequence of a container
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    
    Conflicts:
    	linux-user/main.c
    	vl.c

--- vl.c
+++ vl.c
@@ -3038,6 +3038,8 @@ int main(int argc, char **argv, char **envp)
     }
     rtc_clock = QEMU_CLOCK_HOST;
 
+    qemu_init_auxval(envp);
+
     QLIST_INIT (&vm_change_state_head);
     os_setup_early_signal_handling();
 
