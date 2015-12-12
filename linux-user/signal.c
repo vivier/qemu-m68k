8f94d50f14496516bbcb51901a6c76ff9440e477
Michael Karcher <karcher@physik.fu-berlin.de>
2015-12-12 11:13:08 +0100

    Fix do_rt_sigreturn on m68k linux userspace emulation
    do_rt_sigreturn uses an uninitialised local variable instead of fetching
    the old signal mask directly from the signal frame when restoring the mask,
    so the signal mask is undefined after do_rt_sigreturn. As the signal
    frame data is in target-endian order, target_to_host_sigset instead of
    target_to_host_sigset_internal is required.
    
    do_sigreturn is correct in using target_to_host_sigset_internal, because
    get_user already did the endianness conversion.
    
    Signed-off-by: Michael Karcher <karcher@physik.fu-berlin.de>

--- linux-user/signal.c
+++ linux-user/signal.c
@@ -5288,14 +5288,13 @@ long do_rt_sigreturn(CPUM68KState *env)
 {
     struct target_rt_sigframe *frame;
     abi_ulong frame_addr = env->aregs[7] - 4;
-    target_sigset_t target_set;
     sigset_t set;
     int d0;
 
     if (!lock_user_struct(VERIFY_READ, frame, frame_addr, 1))
         goto badframe;
 
-    target_to_host_sigset_internal(&set, &target_set);
+    target_to_host_sigset(&set, &frame->uc.tuc_sigmask);
     do_sigprocmask(SIG_SETMASK, &set, NULL);
 
     /* restore registers */
