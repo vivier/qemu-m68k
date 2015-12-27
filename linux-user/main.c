b8afb7c335d09483cb8263df6341d903a8904ae8
Laurent Vivier <laurent@vivier.eu>
2015-12-27 16:11:16 +0100

    linux-user,m68k: manage EXCP_DIV0
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/main.c
+++ linux-user/main.c
@@ -3204,6 +3204,13 @@ void cpu_loop(CPUM68KState *env)
             info._sifields._sigfault._addr = env->pc;
             queue_signal(env, info.si_signo, &info);
             break;
+        case EXCP_DIV0:
+            info.si_signo = TARGET_SIGFPE;
+            info.si_errno = 0;
+            info.si_code = TARGET_FPE_INTDIV;
+            info._sifields._sigfault._addr = env->pc;
+            queue_signal(env, info.si_signo, &info);
+            break;
         case EXCP_TRAP0:
             {
                 ts->sim_syscalls = 0;
