86a5b4d115fbe0da020ace9bc58af5763c807ba8
Laurent Vivier <laurent@vivier.eu>
2017-11-20 15:04:35 +0100

    softfloat: add floatx80_atan()
    copied from previous:
    Written by Andreas Grabher for Previous, NeXT Computer Emulator.
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- include/fpu/softfloat.h
+++ include/fpu/softfloat.h
@@ -644,6 +644,7 @@ floatx80 floatx80_tentox(floatx80 a, float_status *status);
 floatx80 floatx80_tan(floatx80 a, float_status *status);
 floatx80 floatx80_sin(floatx80 a, float_status *status);
 floatx80 floatx80_cos(floatx80 a, float_status *status);
+floatx80 floatx80_atan(floatx80 a, float_status *status);
 int floatx80_eq(floatx80, floatx80, float_status *status);
 int floatx80_le(floatx80, floatx80, float_status *status);
 int floatx80_lt(floatx80, floatx80, float_status *status);
