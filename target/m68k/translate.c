9ac35808467fe155a6e4e8b99ea03b9dc67ca92e
Laurent Vivier <laurent@vivier.eu>
2017-11-21 11:31:23 +0100

    target/m68k: implement ftan
    using floatx80_tan()
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target/m68k/translate.c
+++ target/m68k/translate.c
@@ -4662,6 +4662,9 @@ DISAS_INSN(fpu)
     case 0x06: /* flognp1 */
         gen_helper_flognp1(cpu_env, cpu_dest, cpu_src);
         break;
+    case 0x0f: /* ftan */
+        gen_helper_ftan(cpu_env, cpu_dest, cpu_src);
+        break;
     case 0x10: /* fetox */
         gen_helper_fetox(cpu_env, cpu_dest, cpu_src);
         break;
