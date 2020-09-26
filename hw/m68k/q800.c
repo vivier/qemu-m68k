elegantchaos/qemu

q800: add ROM overlay
Signed-off-by: Laurent Vivier <laurent@vivier.eu>

hw/m68k/q800.c
@@ -92,8 +92,18 @@ static void main_cpu_reset(void *opaque)
 {
     M68kCPU *cpu = opaque;
     CPUState *cs = CPU(cpu);
+    uint8_t *rom;
 
     cpu_reset(cs);
+    rom = rom_ptr(MACROM_ADDR, MACROM_SIZE);
+    if (rom) {
+        /*
+         * Boot from Quadra 800 ROM
+         * This should be managed by the ROM overlay
+         */
+        cpu->env.aregs[7] = ldl_p(rom);
+        cpu->env.pc = ldl_p(rom + 4);
+   }
 }
 
 static void q800_init(MachineState *machine)
@@ -106,7 +116,7 @@ static void q800_init(MachineState *machine)
     int bios_size;
     ram_addr_t initrd_base;
     int32_t initrd_size;
-    MemoryRegion *rom;
+    MemoryRegion *rom = NULL;
     MemoryRegion *ram;
     MemoryRegion *io;
     const int io_slice_nb = (IO_SIZE / IO_SLICE) - 1;
@@ -143,6 +153,34 @@ static void q800_init(MachineState *machine)
     memory_region_init_ram(ram, NULL, "m68k_mac.ram", ram_size, &error_abort);
     memory_region_add_subregion(get_system_memory(), 0, ram);
 
+    /* ROM */
+    if (!linux_boot) {
+        if (bios_name == NULL) {
+            bios_name = MACROM_FILENAME;
+        }
+        /* allocate and load BIOS */
+        rom = g_malloc(sizeof(*rom));
+        memory_region_init_rom_nomigrate(rom, NULL, "m68k_mac.rom", MACROM_SIZE,
+                               &error_abort);
+        memory_region_add_subregion(get_system_memory(), MACROM_ADDR, rom);
+        /* Load MacROM binary */
+        filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
+        if (filename) {
+            bios_size = load_image_targphys(filename, MACROM_ADDR, MACROM_SIZE);
+            g_free(filename);
+        } else {
+            bios_size = -1;
+        }
+
+        /* Remove qtest_enabled() check once firmware files are in the tree */
+        if (!qtest_enabled()) {
+            if (bios_size < 0 || bios_size > MACROM_SIZE) {
+                error_report("could not load MacROM '%s'", bios_name);
+                exit(1);
+            }
+        }
+    }
+
     /*
      * Memory from IO_BASE to IO_BASE + IO_SLICE is repeated
      * from IO_BASE + IO_SLICE to IO_BASE + IO_SIZE
@@ -169,6 +207,8 @@ static void q800_init(MachineState *machine)
     /* VIA */
 
     via_dev = qdev_create(NULL, TYPE_MAC_VIA);
+    object_property_set_link(OBJECT(via_dev), OBJECT(rom), "rom_mr",
+                             &error_abort);
     qdev_init_nofail(via_dev);
     sysbus = SYS_BUS_DEVICE(via_dev);
     sysbus_mmio_map(sysbus, 0, VIA_BASE);
@@ -345,39 +385,6 @@ static void q800_init(MachineState *machine)
             initrd_size = 0;
         }
         BOOTINFO0(cs->as, parameters_base, BI_LAST);
-    } else {
-        uint8_t *ptr;
-        /* allocate and load BIOS */
-        rom = g_malloc(sizeof(*rom));
-        memory_region_init_ram(rom, NULL, "m68k_mac.rom", MACROM_SIZE,
-                               &error_abort);
-        if (bios_name == NULL) {
-            bios_name = MACROM_FILENAME;
-        }
-        filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
-        memory_region_set_readonly(rom, true);
-        memory_region_add_subregion(get_system_memory(), MACROM_ADDR, rom);
-
-        /* Load MacROM binary */
-        if (filename) {
-            bios_size = load_image_targphys(filename, MACROM_ADDR, MACROM_SIZE);
-            g_free(filename);
-        } else {
-            bios_size = -1;
-        }
-
-        /* Remove qtest_enabled() check once firmware files are in the tree */
-        if (!qtest_enabled()) {
-            if (bios_size < 0 || bios_size > MACROM_SIZE) {
-                error_report("could not load MacROM '%s'", bios_name);
-                exit(1);
-            }
-
-            ptr = rom_ptr(MACROM_ADDR, MACROM_SIZE);
-            stl_phys(cs->as, 0, ldl_p(ptr));    /* reset initial SP */
-            stl_phys(cs->as, 4,
-                     MACROM_ADDR + ldl_p(ptr + 4)); /* reset initial PC */
-        }
     }
 }
 

hw/misc/mac_via.c
@@ -27,7 +27,8 @@
 #include "sysemu/runstate.h"
 #include "qapi/error.h"
 #include "qemu/cutils.h"
-
+#include "exec/address-spaces.h"
+#include "hw/qdev-properties.h"
 
 /*
  * VIAs: There are two in every machine,
@@ -782,6 +783,16 @@ static void mac_via_realize(DeviceState *dev, Error **errp)
     m->adb_poll_timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, via_adb_poll, m);
     m->adb_data_ready = qdev_get_gpio_in_named(dev, "via1-irq",
                                                VIA1_IRQ_ADB_READY_BIT);
+
+    /* ROM overlay */
+    if (m->rom_mr) {
+        memory_region_init_alias(&m->rom_overlay, NULL, "ROM overlay",
+                                 m->rom_mr, 0,
+                                 memory_region_size(m->rom_mr));
+        memory_region_add_subregion_overlap(get_system_memory(), 0,
+                                            &m->rom_overlay, 1);
+        memory_region_set_enabled(&m->rom_overlay, false);
+    }
 }
 
 static void mac_via_init(Object *obj)
@@ -842,6 +853,12 @@ static const VMStateDescription vmstate_mac_via = {
     }
 };
 
+static Property mac_via_properties[] = {
+    DEFINE_PROP_LINK("rom_mr", MacVIAState, rom_mr,
+                     TYPE_MEMORY_REGION, MemoryRegion *),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
 static void mac_via_class_init(ObjectClass *oc, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(oc);
@@ -849,6 +866,7 @@ static void mac_via_class_init(ObjectClass *oc, void *data)
     dc->realize = mac_via_realize;
     dc->reset = mac_via_reset;
     dc->vmsd = &vmstate_mac_via;
+    dc->props = mac_via_properties;
 }
 
 static TypeInfo mac_via_info = {
@@ -860,6 +878,17 @@ static TypeInfo mac_via_info = {
 };
 
 /* VIA 1 */
+static void mos6522_q800_via1_portA_write(MOS6522State *s)
+{
+    MOS6522Q800VIA1State *v1s = container_of(s, MOS6522Q800VIA1State,
+                                             parent_obj);
+    MacVIAState *m = container_of(v1s, MacVIAState, mos6522_via1);
+
+    if (m->rom_mr && !(s->a & VIA1A_vOverlay)) {
+        memory_region_set_enabled(&m->rom_overlay, false);
+    }
+}
+
 static void mos6522_q800_via1_portB_write(MOS6522State *s)
 {
     MOS6522Q800VIA1State *v1s = container_of(s, MOS6522Q800VIA1State,
@@ -876,12 +905,19 @@ static void mos6522_q800_via1_reset(DeviceState *dev)
 {
     MOS6522State *ms = MOS6522(dev);
     MOS6522DeviceClass *mdc = MOS6522_DEVICE_GET_CLASS(ms);
+    MOS6522Q800VIA1State *v1s = container_of(ms, MOS6522Q800VIA1State,
+                                             parent_obj);
+    MacVIAState *m = container_of(v1s, MacVIAState, mos6522_via1);
 
     mdc->parent_reset(dev);
 
     ms->timers[0].frequency = VIA_TIMER_FREQ;
     ms->timers[1].frequency = VIA_TIMER_FREQ;
 
+    if (m->rom_mr) {
+        ms->a = VIA1A_vOverlay;
+        memory_region_set_enabled(&m->rom_overlay, true);
+    }
     ms->b = VIA1B_vADB_StateMask | VIA1B_vADBInt | VIA1B_vRTCEnb;
 }
 
@@ -897,6 +933,7 @@ static void mos6522_q800_via1_class_init(ObjectClass *oc, void *data)
     MOS6522DeviceClass *mdc = MOS6522_DEVICE_CLASS(oc);
 
     dc->reset = mos6522_q800_via1_reset;
+    mdc->portA_write = mos6522_q800_via1_portA_write;
     mdc->portB_write = mos6522_q800_via1_portB_write;
 }
 

vivier/qemu-m68k

hw/m68k/q800.c
@@ -92,8 +92,18 @@ static void main_cpu_reset(void *opaque)
 {
     M68kCPU *cpu = opaque;
     CPUState *cs = CPU(cpu);
+    uint8_t *rom;
 
     cpu_reset(cs);
+    rom = rom_ptr(MACROM_ADDR, MACROM_SIZE);
+    if (rom) {
+        /*
+         * Boot from Quadra 800 ROM
+         * This should be managed by the ROM overlay
+         */
+        cpu->env.aregs[7] = ldl_p(rom);
+        cpu->env.pc = ldl_p(rom + 4);
+   }
 }
 
 static uint8_t fake_mac_rom[] = {
@@ -127,7 +137,7 @@ static void q800_init(MachineState *machine)
     int bios_size;
     ram_addr_t initrd_base;
     int32_t initrd_size;
-    MemoryRegion *rom;
+    MemoryRegion *rom = NULL;
     MemoryRegion *io;
     const int io_slice_nb = (IO_SIZE / IO_SLICE) - 1;
     int i;
@@ -162,6 +172,34 @@ static void q800_init(MachineState *machine)
     /* RAM */
     memory_region_add_subregion(get_system_memory(), 0, machine->ram);
 
+    /* ROM */
+    if (!linux_boot) {
+        if (bios_name == NULL) {
+            bios_name = MACROM_FILENAME;
+        }
+        /* allocate and load BIOS */
+        rom = g_malloc(sizeof(*rom));
+        memory_region_init_rom(rom, NULL, "m68k_mac.rom", MACROM_SIZE,
+                               &error_abort);
+        memory_region_add_subregion(get_system_memory(), MACROM_ADDR, rom);
+        /* Load MacROM binary */
+        filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
+        if (filename) {
+            bios_size = load_image_targphys(filename, MACROM_ADDR, MACROM_SIZE);
+            g_free(filename);
+        } else {
+            bios_size = -1;
+        }
+
+        /* Remove qtest_enabled() check once firmware files are in the tree */
+        if (!qtest_enabled()) {
+            if (bios_size < 0 || bios_size > MACROM_SIZE) {
+                error_report("could not load MacROM '%s'", bios_name);
+                exit(1);
+            }
+        }
+    }
+
     /*
      * Memory from IO_BASE to IO_BASE + IO_SLICE is repeated
      * from IO_BASE + IO_SLICE to IO_BASE + IO_SIZE
@@ -193,6 +231,8 @@ static void q800_init(MachineState *machine)
     if (dinfo) {
         qdev_prop_set_drive(via_dev, "drive", blk_by_legacy_dinfo(dinfo));
     }
+    object_property_set_link(OBJECT(via_dev), "rom_mr", OBJECT(rom),
+                             &error_abort);
     sysbus = SYS_BUS_DEVICE(via_dev);
     sysbus_realize_and_unref(sysbus, &error_fatal);
     sysbus_mmio_map(sysbus, 0, VIA_BASE);
@@ -375,38 +415,6 @@ static void q800_init(MachineState *machine)
             initrd_size = 0;
         }
         BOOTINFO0(cs->as, parameters_base, BI_LAST);
-    } else {
-        uint8_t *ptr;
-        /* allocate and load BIOS */
-        rom = g_malloc(sizeof(*rom));
-        memory_region_init_rom(rom, NULL, "m68k_mac.rom", MACROM_SIZE,
-                               &error_abort);
-        if (bios_name == NULL) {
-            bios_name = MACROM_FILENAME;
-        }
-        filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
-        memory_region_add_subregion(get_system_memory(), MACROM_ADDR, rom);
-
-        /* Load MacROM binary */
-        if (filename) {
-            bios_size = load_image_targphys(filename, MACROM_ADDR, MACROM_SIZE);
-            g_free(filename);
-        } else {
-            bios_size = -1;
-        }
-
-        /* Remove qtest_enabled() check once firmware files are in the tree */
-        if (!qtest_enabled()) {
-            if (bios_size < 0 || bios_size > MACROM_SIZE) {
-                error_report("could not load MacROM '%s'", bios_name);
-                exit(1);
-            }
-
-            ptr = rom_ptr(MACROM_ADDR, MACROM_SIZE);
-            stl_phys(cs->as, 0, ldl_p(ptr));    /* reset initial SP */
-            stl_phys(cs->as, 4,
-                     MACROM_ADDR + ldl_p(ptr + 4)); /* reset initial PC */
-        }
     }
 }
 

hw/misc/mac_via.c
@@ -31,6 +31,7 @@
 #include "sysemu/block-backend.h"
 #include "trace.h"
 #include "qemu/log.h"
+#include "exec/address-spaces.h"
 
 /*
  * VIAs: There are two in every machine,
@@ -940,6 +941,11 @@ static void mos6522_q800_via1_write(void *opaque, hwaddr addr, uint64_t val,
     mos6522_write(ms, addr, val, size);
 
     switch (addr) {
+    case VIA_REG_A:
+        if (m->rom_mr && !(ms->a & VIA1A_vOverlay)) {
+            memory_region_set_enabled(&m->rom_overlay, false);
+        }
+        break;
     case VIA_REG_B:
         via1_rtc_update(m);
         via1_adb_update(m);
@@ -1073,6 +1079,16 @@ static void mac_via_realize(DeviceState *dev, Error **errp)
             return;
         }
     }
+
+    /* ROM overlay */
+    if (m->rom_mr) {
+        memory_region_init_alias(&m->rom_overlay, NULL, "ROM overlay",
+                                 m->rom_mr, 0,
+                                 memory_region_size(m->rom_mr));
+        memory_region_add_subregion_overlap(get_system_memory(), 0,
+                                            &m->rom_overlay, 1);
+        memory_region_set_enabled(&m->rom_overlay, false);
+    }
 }
 
 static void mac_via_init(Object *obj)
@@ -1158,6 +1174,8 @@ static const VMStateDescription vmstate_mac_via = {
 
 static Property mac_via_properties[] = {
     DEFINE_PROP_DRIVE("drive", MacVIAState, blk),
+    DEFINE_PROP_LINK("rom_mr", MacVIAState, rom_mr,
+                     TYPE_MEMORY_REGION, MemoryRegion *),
     DEFINE_PROP_END_OF_LIST(),
 };
 
@@ -1184,12 +1202,19 @@ static void mos6522_q800_via1_reset(DeviceState *dev)
 {
     MOS6522State *ms = MOS6522(dev);
     MOS6522DeviceClass *mdc = MOS6522_GET_CLASS(ms);
+    MOS6522Q800VIA1State *v1s = container_of(ms, MOS6522Q800VIA1State,
+                                             parent_obj);
+    MacVIAState *m = container_of(v1s, MacVIAState, mos6522_via1);
 
     mdc->parent_reset(dev);
 
     ms->timers[0].frequency = VIA_TIMER_FREQ;
     ms->timers[1].frequency = VIA_TIMER_FREQ;
 
+    if (m->rom_mr) {
+        ms->a = VIA1A_vOverlay;
+        memory_region_set_enabled(&m->rom_overlay, true);
+    }
     ms->b = VIA1B_vADB_StateMask | VIA1B_vADBInt | VIA1B_vRTCEnb;
 }
 